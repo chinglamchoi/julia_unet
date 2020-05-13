d85fdd034ead75af235e685b8706ccb2e9e48e18
Ching Lam Choi <ccl5a09@gmail.com>
2020-05-13 18:46:40 +0800

    Update main_flux2.jl

--- main_flux2.jl
+++ main_flux2.jl
@@ -1,10 +1,11 @@
 using Flux
-using CuArrays
-using Flux: onehotbatch, onecold, crossentropy, throttle
+using Flux: logitbinarycrossentropy, throttle
 using Base.Iterators: partition
-using CSV, FileIO
+using CSV
 using Images
+using Statistics: mean
 using BSON
+using CuArrays, CUDAnative
 CuArrays.allowscalar(false)
 include("unet_flux2.jl")
 
@@ -14,7 +15,7 @@ mask_train_path, mask_test_path = "C:/Users/CCL/unet/TCGA/masks/train/new/", "C:
 train, test = gpu.(collect(CSV.read("C:/Users/CCL/unet/TCGA/imgs/train.csv", header=["fname"]).fname)), gpu.(collect(CSV.read("C:/Users/CCL/unet/TCGA/imgs/test.csv", header=["fname"]).fname))
 train_size, test_size = length(train), length(test)
 
-mb_size = 80
+mb_size = 8
 mb_idxs = collect(partition(1:train_size, mb_size))
 
 function load_me(img_path, mask_path, path, idxs, testing)
@@ -26,35 +27,31 @@ function load_me(img_path, mask_path, path, idxs, testing)
         y2[:, :, :, cnt] = testing ? reshape(Float32.(channelview(load(mask_path.*path[i]))), (256, 256, 1)) : reshape((Float32.(channelview(load(mask_path*path[i])))[1, :, :]), (256, 256, 1))
         cnt += 1
     end
-    return (y1, y2)
+    return [(y1, y2)]
 end
 
-load_me = gpu(load_me)
-UNet = gpu(UNet)
+UNet_model = UNet() |> gpu
 
-# trainset = [cpu(load_me(img_train_path, mask_train_path, train, i, false)) for i in mb_idxs]
-
-loss(x, y) = logitbinarycrossentropy(UNet(x), y) |> gpu
+loss(x, y) = mean(logitbinarycrossentropy.(UNet_model(x) |> gpu, y))
 
 function accuracy(x,y)
-    y_hat = UNet(x)
+    y_hat = UNet_model(x)
     return 2 * sum(y_hat .* y) / (sum(y_hat) + sum(y))
 end
-accuracy = gpu(accuracy)
 
 optimiser = ADAM()
 best_acc, last_improve, epoch_num, threshold = 0.0, 0, 200, 0.95
 
 for i in 1:epoch_num
     for o in 1:length(mb_idxs)
-        train_batch = load_me(img_train_path, mask_train_path, train, mb_idxs[o], false)
-        println(params(UNet))
-        Flux.train!(loss, params(UNet), train_batch, optimiser)
+        train_batch = gpu.(load_me(img_train_path, mask_train_path, train, mb_idxs[o], false))
+        Flux.train!(loss, params(UNet_model), train_batch, optimiser)
+        println("trained")
     end
     testset = gpu.(load_me(img_test_path, mask_test_path, test, collect(1:length(test)), true))
     acc = accuracy(testset...)
     if acc > best_acc
-        model = cpu(UNet)
+        model = cpu(UNet_model)
         BSON.@save "best_model.BSON" model
         global best_acc = acc
         println("New best accuracy!")
